41b24b792ba877956d65d31669afcab445e3de9f
eduardo-ferroni <eduard.ferroni@hp.com>
2022-10-18 09:52:37 -0300

    implement endpoint for list and create products

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -29,4 +29,51 @@ describe("E2E test for customer", () => {
     expect(response.body.address.zip).toBe("Zip");
     expect(response.body.address.city).toBe("City");
   });
+
+  it("should not create a customer", async () => {
+    const response = await request(app).post("/customers").send({
+      name: "John",
+    });
+    expect(response.status).toBe(500);
+  });
+
+  it("should list all customers", async () => {
+    const response1 = await request(app)
+      .post("/customers")
+      .send({
+        name: "John",
+        address: {
+          street: "Street 1",
+          number: 1,
+          zip: "Zip 1",
+          city: "City 1",
+        },
+      });
+    expect(response1.status).toBe(200);
+
+    const response2 = await request(app)
+      .post("/customers")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          number: 2,
+          zip: "Zip 2",
+          city: "City 2",
+        },
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/customers").send();
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+
+    const customer1 = listResponse.body.customers[0];
+    expect(customer1.name).toBe("John");
+    expect(customer1.address.street).toBe("Street 1");
+
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jane");
+    expect(customer2.address.street).toBe("Street 2");
+  });
 });
